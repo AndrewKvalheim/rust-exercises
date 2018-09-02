d60554911efbf64653ad9aced6cec52b3f0d5b8e
Andrew Kvalheim <Andrew@Kvalhe.im>
2018-09-02 12:00:00 -0700

    Solve saddle-points.

--- saddle-points/src/lib.rs
+++ saddle-points/src/lib.rs
@@ -1,6 +1,12 @@
-pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    unimplemented!(
-        "find the saddle points of the following matrix: {:?}",
-        input
-    )
+pub fn find_saddle_points(rows: &[Vec<u64>]) -> Vec<(usize, usize)> {
+    rows.iter()
+        .enumerate()
+        .flat_map(|(i, row)| {
+            row.iter().max().into_iter().flat_map(move |max| {
+                row.iter()
+                    .enumerate()
+                    .filter(move |&(j, x)| x == max && rows.iter().all(|row| row[j] >= *max))
+                    .map(move |(j, _)| (i, j))
+            })
+        }).collect()
 }
