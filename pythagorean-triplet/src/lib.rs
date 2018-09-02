c0f57f45193225be22f4720783ea920202926d3b
Andrew Kvalheim <Andrew@Kvalhe.im>
2018-09-01 18:30:00 -0700

    Solve pythagorean-triplet.

--- pythagorean-triplet/src/lib.rs
+++ pythagorean-triplet/src/lib.rs
@@ -1,3 +1,12 @@
+const PERIMETER: u32 = 1000;
+
 pub fn find() -> Option<u32> {
-    unimplemented!();
+    (1..=(PERIMETER / 2))
+        .map(|a| {
+            let b = (PERIMETER.pow(2) / 2 - PERIMETER * a) / (PERIMETER - a);
+            let c = PERIMETER - (a + b);
+
+            (a, b, c)
+        }).find(|(a, b, c)| a.pow(2) + b.pow(2) == c.pow(2))
+        .map(|(a, b, c)| a * b * c)
 }
