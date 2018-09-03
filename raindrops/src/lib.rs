34e639fa634e6f0d8c24280714697323161a7b32
Andrew Kvalheim <Andrew@Kvalhe.im>
2018-09-03 12:20:00 -0700

    Solve raindrops.

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,3 +1,14 @@
-pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+const CODE: &[(usize, &str)] = &[(3, "Pling"), (5, "Plang"), (7, "Plong")];
+
+pub fn raindrops(number: usize) -> String {
+    let sound = CODE
+        .iter()
+        .filter_map(|&(f, p)| if number % f == 0 { Some(p) } else { None })
+        .collect::<String>();
+
+    if sound.is_empty() {
+        number.to_string()
+    } else {
+        sound
+    }
 }
