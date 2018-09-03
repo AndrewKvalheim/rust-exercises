10a28fbdb1890efdd90e4634159923a0b3b8192b
Andrew Kvalheim <Andrew@Kvalhe.im>
2018-09-03 13:30:00 -0700

    Solve reverse-string.

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,3 +1,14 @@
-pub fn reverse(input: &str) -> String {
-    unimplemented!("Write a function to reverse {}", input);
+#[cfg(feature = "grapheme")]
+extern crate grapheme;
+
+#[cfg(feature = "grapheme")]
+use grapheme::UnicodeSegmentation;
+
+pub fn reverse(text: &str) -> String {
+    #[cfg(feature = "grapheme")]
+    let parts = UnicodeSegmentation::graphemes(text, true);
+    #[cfg(not(feature = "grapheme"))]
+    let parts = text.chars();
+
+    parts.rev().collect()
 }
