e05145e260c158926f45803b63399e4d5381b2a0
Andrew Kvalheim <Andrew@Kvalhe.im>
2018-09-05 15:00:00 -0700

    Solve bob.

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,3 +1,44 @@
+#[macro_use]
+extern crate lazy_static;
+extern crate regex;
+
+use regex::Regex;
+
+lazy_static! {
+    static ref MESSAGE_PATTERN: Regex = Regex::new(
+        r"(?sx)^
+
+            # Optional whitespace
+            \s*
+
+            # Message
+            (?:
+                # Shouting
+                (?P<shouting> [^\p{Lowercase}]*? \p{Uppercase} [^\p{Lowercase}]*? )
+
+                # or non-shouting
+                | .*? \S .*?
+            )
+
+            # Optional question mark
+            (?P<question>\?)?
+
+            # Optional whitespace
+            \s*
+
+            $",
+    )
+    .unwrap();
+}
+
 pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {}", message)
+    match MESSAGE_PATTERN.captures(message) {
+        None => "Fine. Be that way!",
+        Some(captures) => match (captures.name("shouting"), captures.name("question")) {
+            (None, None) => "Whatever.",
+            (None, Some(_)) => "Sure.",
+            (Some(_), None) => "Whoa, chill out!",
+            (Some(_), Some(_)) => "Calm down, I know what I'm doing!",
+        },
+    }
 }
