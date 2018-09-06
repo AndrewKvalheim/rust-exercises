bdff554ef1ed508a636b6e09d7cdd62e84c4d2e5
Andrew Kvalheim <Andrew@Kvalhe.im>
2018-09-06 14:00:00 -0700

    Solve proverb.

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,3 +1,16 @@
-pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+pub fn build_proverb(items: &[&str]) -> String {
+    items
+        .windows(2)
+        .map(|pair| match pair {
+            [wanted, lost] => format!("For want of a {} the {} was lost.", wanted, lost),
+            _ => unreachable!(),
+        })
+        .chain(
+            items
+                .iter()
+                .take(1)
+                .map(|wanted| format!("And all for the want of a {}.", wanted)),
+        )
+        .collect::<Vec<_>>()
+        .join("\n")
 }
