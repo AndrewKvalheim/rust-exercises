b320bcff4c769e3e200bbb4c8858315b2a5fcf7c
Andrew Kvalheim <Andrew@Kvalhe.im>
2018-09-06 20:40:00 -0700

    Solve collatz-conjecture.

--- collatz-conjecture/src/lib.rs
+++ collatz-conjecture/src/lib.rs
@@ -1,6 +1,20 @@
 pub fn collatz(n: u64) -> Option<u64> {
-    unimplemented!(
-        "return Some(x) where x is the number of steps required to reach 1 starting with {}",
-        n,
-    )
+    n.checked_sub(1).map(|m| Collatz(m).count() as u64)
+}
+
+struct Collatz(u64);
+
+impl Iterator for Collatz {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match 1 + self.0 {
+            1 => None,
+            n => {
+                self.0 = if n % 2 == 0 { n / 2 } else { 3 * n + 1 } - 1;
+
+                Some(n)
+            }
+        }
+    }
 }
