6c0e8bc7360c18346b48094a8beda37525c57843
Andrew Kvalheim <Andrew@Kvalhe.im>
2018-09-06 21:30:00 -0700

    Solve diffie-hellman.

--- diffie-hellman/src/lib.rs
+++ diffie-hellman/src/lib.rs
@@ -1,21 +1,18 @@
+extern crate rand;
+
+mod utilities;
+
+use utilities::modular_pow;
+use rand::prelude::*;
+
 pub fn private_key(p: u64) -> u64 {
-    unimplemented!("Pick a private key greater than 1 and less than {}", p)
+    thread_rng().gen_range(2, p)
 }
 
-pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
-    unimplemented!(
-        "Calculate public key using prime numbers {} and {}, and private key {}",
-        p,
-        g,
-        a
-    )
+pub fn public_key(p: u64, g: u64, private_key: u64) -> u64 {
+    modular_pow(g, private_key, p)
 }
 
-pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
-    unimplemented!(
-        "Calculate secret key using prime number {}, public key {}, and private key {}",
-        p,
-        b_pub,
-        a
-    )
+pub fn secret(p: u64, public_key: u64, private_key: u64) -> u64 {
+    modular_pow(public_key, private_key, p)
 }
---
+++ diffie-hellman/src/utilities.rs
@@ -0,0 +1,16 @@
+// https://en.wikipedia.org/wiki/Modular_exponentiation#Right-to-left_binary_method
+pub fn modular_pow(base: u64, exponent: u64, modulus: u64) -> u64 {
+    let mut cycle_base = base % modulus;
+    let mut cycle_exponent = exponent;
+    let mut result = 1;
+
+    while cycle_exponent > 0 {
+        if cycle_exponent % 2 == 1 {
+            result = (result * cycle_base) % modulus;
+        }
+        cycle_base = cycle_base.pow(2) % modulus;
+        cycle_exponent >>= 1;
+    }
+
+    result
+}
