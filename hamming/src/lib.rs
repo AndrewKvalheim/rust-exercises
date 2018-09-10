95c2e2756fdccef8b47d16a8968df7205bc179d0
Andrew Kvalheim <Andrew@Kvalhe.im>
2018-09-10 15:00:00 -0700

    Solve hamming.

--- hamming/src/lib.rs
+++ hamming/src/lib.rs
@@ -1,5 +1,5 @@
-/// Return the Hamming distance between the strings,
-/// or None if the lengths are mismatched.
-pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
-    unimplemented!("What is the Hamming Distance between {} and {}", s1, s2);
+pub fn hamming_distance(sequence_1: &str, sequence_2: &str) -> Option<usize> {
+    Some((sequence_1, sequence_2))
+        .filter(|(s1, s2)| s1.len() == s2.len())
+        .map(|(s1, s2)| s1.chars().zip(s2.chars()).filter(|(a, b)| a != b).count())
 }
