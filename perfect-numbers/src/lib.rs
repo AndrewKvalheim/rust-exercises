7b4511b45f16d8ca92e4d8182a2e8bf14a1f856e
Andrew Kvalheim <Andrew@Kvalhe.im>
2018-09-10 14:00:00 -0700

    Solve perfect-numbers.

--- perfect-numbers/src/lib.rs
+++ perfect-numbers/src/lib.rs
@@ -1,10 +1,24 @@
-#[derive(Debug, PartialEq, Eq)]
+use std::cmp::Ordering::{Equal, Greater, Less};
+use Classification::{Abundant, Deficient, Perfect};
+
+#[derive(Debug, PartialEq)]
 pub enum Classification {
     Abundant,
-    Perfect,
     Deficient,
+    Perfect,
 }
 
-pub fn classify(num: u64) -> Option<Classification> {
-    unimplemented!("classify {}", num);
+pub fn classify(n: u64) -> Option<Classification> {
+    Some(match aliquot_sum(n)?.cmp(&n) {
+        Equal => Perfect,
+        Greater => Abundant,
+        Less => Deficient,
+    })
+}
+
+fn aliquot_sum(n: u64) -> Option<u64> {
+    match n {
+        0 => None,
+        _ => Some((1..=(n / 2)).filter(|m| n % m == 0).sum()),
+    }
 }
