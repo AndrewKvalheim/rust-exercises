3592eb4666d65bc8913bc28ed847c5c4d6940beb
Andrew Kvalheim <Andrew@Kvalhe.im>
2018-09-10 11:30:00 -0700

    Solve isbn-verifier.

--- isbn-verifier/src/lib.rs
+++ isbn-verifier/src/lib.rs
@@ -1,4 +1,12 @@
-/// Determines whether the supplied string is a valid ISBN number
 pub fn is_valid_isbn(isbn: &str) -> bool {
-    unimplemented!("Is {:?} a valid ISBN number?", isbn);
+    isbn.chars()
+        .filter(|&character| character != '-')
+        .enumerate()
+        .flat_map(|indexed_numeral| match indexed_numeral {
+            (9, 'X') => Some(1),
+            (index, numeral) => numeral.to_digit(10).map(|d| d * (1 + index as u32)),
+        })
+        .try_fold((0, 0), |(count, sum), digit| Some((count + 1, sum + digit)))
+        .filter(|&(count, sum)| count == 10 && sum % 11 == 0)
+        .is_some()
 }
