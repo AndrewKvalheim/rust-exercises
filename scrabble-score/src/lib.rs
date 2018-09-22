4547c5eec41442c6fc7329e549efe48a68215191
Andrew Kvalheim <Andrew@Kvalhe.im>
2018-09-22 16:55:00 -0700

    Solve scrabble-score.

--- scrabble-score/src/lib.rs
+++ scrabble-score/src/lib.rs
@@ -1,4 +1,16 @@
-/// Compute the Scrabble score for a word.
 pub fn score(word: &str) -> u64 {
-    unimplemented!("Score {} in Scrabble.", word);
+    word.chars().map(value).sum()
+}
+
+fn value(letter: char) -> u64 {
+    match letter.to_ascii_uppercase() {
+        'A' | 'E' | 'I' | 'L' | 'N' | 'O' | 'R' | 'S' | 'T' | 'U' => 1,
+        'D' | 'G' => 2,
+        'B' | 'C' | 'M' | 'P' => 3,
+        'F' | 'H' | 'V' | 'W' | 'Y' => 4,
+        'K' => 5,
+        'J' | 'X' => 8,
+        'Q' | 'Z' => 10,
+        _ => 0,
+    }
 }
