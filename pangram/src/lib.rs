d91c56b0bd4ee8bc46c12d08b9a0e1895916ba01
Andrew Kvalheim <Andrew@Kvalhe.im>
2018-09-22 18:00:00 -0700

    Solve pangram.

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -1,4 +1,9 @@
-/// Determine whether a sentence is a pangram.
-pub fn is_pangram(sentence: &str) -> bool {
-    unimplemented!("Is {} a pangram?", sentence);
+pub fn is_pangram(text: &str) -> bool {
+    text.chars()
+        .filter(|c| c.is_ascii_alphabetic())
+        .fold([false; 26], |mut seen, letter| {
+            seen[letter.to_ascii_uppercase() as usize - b'A' as usize] = true;
+            seen
+        }).iter()
+        .all(|&b| b)
 }
