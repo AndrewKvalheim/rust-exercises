33d6b4bdf8eb1bc5f931f40629a4adb4b92b8579
Andrew Kvalheim <Andrew@Kvalhe.im>
2018-09-25 18:15:00 -0700

    Solve crypto-square.

--- crypto-square/src/lib.rs
+++ crypto-square/src/lib.rs
@@ -1,3 +1,21 @@
-pub fn encrypt(input: &str) -> String {
-    unimplemented!("Encrypt {:?} using a square code", input)
+pub fn encrypt(text: &str) -> String {
+    let characters = text
+        .chars()
+        .filter(|&c| c.is_alphanumeric())
+        .flat_map(|c| c.to_lowercase())
+        .collect::<Vec<_>>();
+
+    let (m, n) = {
+        let ideal = (characters.len() as f64).sqrt();
+
+        (ideal.ceil() as usize, ideal.floor() as usize)
+    };
+
+    let row = |i| {
+        (0..n)
+            .map(|j| characters.get(i + j * m).unwrap_or(&' '))
+            .collect()
+    };
+
+    (0..m).map(row).collect::<Vec<String>>().join(" ")
 }
