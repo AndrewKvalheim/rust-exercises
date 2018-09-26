67ae1794a50801378dea730ee45f52757a35ed5a
Andrew Kvalheim <Andrew@Kvalhe.im>
2018-09-25 21:00:00 -0700

    Solve acronym.

--- acronym/src/lib.rs
+++ acronym/src/lib.rs
@@ -1,3 +1,18 @@
-pub fn abbreviate(phrase: &str) -> String {
-    unimplemented!("Given the phrase '{}', return its acronym", phrase);
+pub fn abbreviate(term: &str) -> String {
+    term.chars()
+        .scan(None::<char>, |previous, character| {
+            let letter = match previous {
+                None => Some(character),
+                Some(p) if !p.is_alphabetic() && character.is_alphabetic() => Some(character),
+                Some(p) if p.is_lowercase() && character.is_uppercase() => Some(character),
+                _ => None,
+            };
+
+            *previous = Some(character);
+
+            Some(letter)
+        })
+        .filter_map(/* Pending RFC 2306 */ |l| l)
+        .flat_map(char::to_uppercase)
+        .collect()
 }
