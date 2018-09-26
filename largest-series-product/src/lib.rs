472d68644ce2a7d4ea885e779853db9ecc889ace
Andrew Kvalheim <Andrew@Kvalhe.im>
2018-09-25 17:30:00 -0700

    Solve largest-series-product.

--- largest-series-product/src/lib.rs
+++ largest-series-product/src/lib.rs
@@ -1,13 +1,44 @@
+use std::collections::VecDeque;
+use Error::*;
+
 #[derive(Debug, PartialEq)]
 pub enum Error {
-    SpanTooLong,
     InvalidDigit(char),
+    SpanTooLong,
 }
 
-pub fn lsp(string_digits: &str, span: usize) -> Result<u64, Error> {
-    unimplemented!(
-        "largest series product of a span of {} digits in {}",
-        span,
-        string_digits
-    );
+pub fn lsp(numeral: &str, length: usize) -> Result<u64, Error> {
+    if length > numeral.len() {
+        Err(SpanTooLong)
+    } else {
+        numeral
+            .chars()
+            .try_fold(
+                (None, VecDeque::with_capacity(length)),
+                |(largest, mut series), character| {
+                    let digit = character
+                        .to_digit(10)
+                        .ok_or_else(|| InvalidDigit(character))?
+                        .into();
+
+                    series.push_back(digit);
+
+                    let next = if series.len() == length {
+                        let product = series.iter().product();
+
+                        series.pop_front();
+
+                        match largest {
+                            Some(l) if l >= product => largest,
+                            _ => Some(product),
+                        }
+                    } else {
+                        largest
+                    };
+
+                    Ok((next, series))
+                },
+            )
+            .map(|(largest, _)| largest.unwrap_or(1))
+    }
 }
