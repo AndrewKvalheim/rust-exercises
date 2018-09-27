700b21d55516998f0d46594f082c7ee9f9c9f5e5
Andrew Kvalheim <Andrew@Kvalhe.im>
2018-09-27 10:30:00 -0700

    Solve binary-search.

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,7 +1,12 @@
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    unimplemented!(
-        "Using the binary search algorithm, find the element '{}' in the array '{:?}' and return its index.",
-        key,
-        array
-    );
+use std::cmp::Ordering::*;
+
+pub fn find<T: Ord, Ts: AsRef<[T]>>(items: Ts, item: T) -> Option<usize> {
+    let items = items.as_ref();
+    let index = items.len() / 2;
+
+    match item.cmp(items.get(index)?) {
+        Equal => Some(index),
+        Greater => find(&items[index + 1..], item).map(|f| index + 1 + f),
+        Less => find(&items[..index], item),
+    }
 }
