6fa11e7eb4abb2d92dbef36df4fa99d26b285bbf
Andrew Kvalheim <Andrew@Kvalhe.im>
2019-01-06 21:30:00 -0800

    Solve bracket-push.

--- bracket-push/src/lib.rs
+++ bracket-push/src/lib.rs
@@ -1,6 +1,22 @@
-pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!(
-        "Check if the string \"{}\" contains balanced brackets",
-        string
-    );
+const OPENINGS: [char; 3] = ['(', '[', '{'];
+const CLOSINGS: [char; 3] = [')', ']', '}'];
+
+pub fn brackets_are_balanced(text: &str) -> bool {
+    text.chars()
+        .try_fold(Vec::new(), |mut due, character| {
+            if let Some(&closing) = CLOSINGS.iter().find(|&&c| c == character) {
+                if due.pop() == Some(closing) {
+                    Some(due)
+                } else {
+                    None
+                }
+            } else {
+                if let Some(index) = OPENINGS.iter().position(|&o| o == character) {
+                    due.push(CLOSINGS[index]);
+                }
+
+                Some(due)
+            }
+        })
+        .map_or(false, |due| due.is_empty())
 }
