fa51d0412ef280e2f07708cae7913c5328f1b2f2
Andrew Kvalheim <Andrew@Kvalhe.im>
2019-01-08 09:00:00 -0800

    Solve run-length-encoding.

--- run-length-encoding/src/lib.rs
+++ run-length-encoding/src/lib.rs
@@ -1,7 +1,48 @@
-pub fn encode(source: &str) -> String {
-    unimplemented!("Return the run-length encoding of {}.", source);
+use std::iter::{once, repeat};
+
+pub fn decode(code: &str) -> String {
+    code.chars()
+        .scan(String::new(), |numeral, character| {
+            let length = if character.is_numeric() {
+                numeral.push(character);
+
+                0
+            } else {
+                let number = numeral.parse().unwrap_or(1);
+
+                *numeral = String::new();
+
+                number
+            };
+
+            Some(repeat(character).take(length))
+        })
+        .flatten()
+        .collect()
 }
 
-pub fn decode(source: &str) -> String {
-    unimplemented!("Return the run-length decoding of {}.", source);
+pub fn encode(text: &str) -> String {
+    text.chars()
+        .map(Some)
+        .chain(once(None))
+        .scan(None, |run, character| {
+            let instruction = match (&run, character) {
+                (Some((previous, _)), Some(c)) if *previous == c => None,
+                (Some((previous, length)), _) => Some(match length {
+                    1 => format!("{}", previous),
+                    _ => format!("{}{}", length, previous),
+                }),
+                _ => None,
+            };
+
+            match (run, character) {
+                (Some((previous, length)), Some(c)) if *previous == c => *length += 1,
+                (r, Some(c)) => *r = Some((c, 1)),
+                _ => (),
+            };
+
+            Some(instruction)
+        })
+        .flatten()
+        .collect()
 }
