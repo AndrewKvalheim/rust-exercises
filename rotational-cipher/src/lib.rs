912eedeeab9b01c5fd448149e47ad045a4706079
Andrew Kvalheim <Andrew@Kvalhe.im>
2019-01-19 14:00:00 -0800

    Solve rotational-cipher.

--- rotational-cipher/src/lib.rs
+++ rotational-cipher/src/lib.rs
@@ -1,7 +1,11 @@
-pub fn rotate(input: &str, key: i8) -> String {
-    unimplemented!(
-        "How would input text '{}' transform when every letter is shifted using key '{}'?",
-        input,
-        key
-    );
+pub fn rotate(text: &str, key: u8) -> String {
+    let substitute = |start, end, c| (start + (c as u8 - start + key) % (end - start + 1)) as char;
+
+    text.chars()
+        .map(|character| match character {
+            'A'...'Z' => substitute(b'A', b'Z', character),
+            'a'...'z' => substitute(b'a', b'z', character),
+            _ => character,
+        })
+        .collect()
 }
