17588d281f6984223d560bc3a833cb56ba2a05cb
Andrew Kvalheim <Andrew@Kvalhe.im>
2019-01-19 17:37:00 -0800

    Solve simple-cipher.

--- simple-cipher/src/lib.rs
+++ simple-cipher/src/lib.rs
@@ -1,14 +1,45 @@
-pub fn encode(key: &str, s: &str) -> Option<String> {
-    unimplemented!("Use {} to encode {} using shift cipher", key, s)
+extern crate rand;
+
+use rand::{thread_rng, Rng};
+use std::ops::{Add, Sub};
+
+pub fn decode(key: &str, ciphertext: &str) -> Option<String> {
+    substitute(key, ciphertext, u8::sub)
 }
 
-pub fn decode(key: &str, s: &str) -> Option<String> {
-    unimplemented!("Use {} to decode {} using shift cipher", key, s)
+pub fn encode(key: &str, plaintext: &str) -> Option<String> {
+    substitute(key, plaintext, u8::add)
 }
 
-pub fn encode_random(s: &str) -> (String, String) {
-    unimplemented!(
-        "Generate random key with only a-z chars and encode {}. Return tuple (key, encoded s)",
-        s
-    )
+pub fn encode_random(plaintext: &str) -> (String, String) {
+    let key = generate_key(100);
+    let ciphertext = encode(&key, plaintext).expect("failed to encode");
+
+    (key, ciphertext)
+}
+
+fn generate_key(length: usize) -> String {
+    let mut rng = thread_rng();
+
+    (0..length)
+        .map(|_| rng.gen_range(b'a', 1 + b'z') as char)
+        .collect()
+}
+
+fn substitute<F>(key: &str, plaintext: &str, operation: F) -> Option<String>
+where
+    F: Fn(u8, u8) -> u8,
+{
+    plaintext
+        .chars()
+        .zip(key.chars().cycle())
+        .map(|(c, k)| {
+            if c.is_ascii_lowercase() && k.is_ascii_lowercase() {
+                Some((b'a' + operation(26 + c as u8 - b'a', k as u8 - b'a') % 26) as char)
+            } else {
+                None
+            }
+        })
+        .collect::<Option<String>>()
+        .filter(|s| !s.is_empty())
 }
