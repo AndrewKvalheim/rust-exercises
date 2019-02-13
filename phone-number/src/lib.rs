d17dc30e76598d9af2b5526df6cad691b0173793
Andrew Kvalheim <Andrew@Kvalhe.im>
2019-02-13 07:35:36 -0800

    Solve phone-number.

--- phone-number/src/lib.rs
+++ phone-number/src/lib.rs
@@ -1,6 +1,21 @@
-pub fn number(user_number: &str) -> Option<String> {
-    unimplemented!(
-        "Given the number entered by user '{}', convert it into SMS-friendly format. If the entered number is not a valid NANP number, return None.",
-        user_number
-    );
+use std::iter::once;
+
+pub fn number(input: &str) -> Option<String> {
+    input
+        .chars()
+        .filter(|c| c.is_numeric())
+        .map(Some)
+        .chain(once(None))
+        .scan(0, |position, next| {
+            *position += if *position == 0 && next != Some('1') { 2 } else { 1 };
+
+            match (next, *position) {
+                (Some('1'), 1) => Some(None), /* Omit */
+                (Some('0'...'1'), _) if [2, 5].contains(position) => Some(Some(None)), /* Fail */
+                (_, 12) => next.map(|_| Some(None)), /* Stop or fail */
+                _ => Some(Some(next)), /* Emit */
+            }
+        })
+        .flatten()
+        .collect()
 }
