d0e1f545fa63b4f023015dcbb8427b7c139257e6
Andrew Kvalheim <Andrew@Kvalhe.im>
2019-02-18 12:40:39 -0800

    Solve diamond.

--- diamond/src/lib.rs
+++ diamond/src/lib.rs
@@ -1,6 +1,12 @@
-pub fn get_diamond(c: char) -> Vec<String> {
-    unimplemented!(
-        "Return the vector of strings which represent the diamond with particular char {}",
-        c
-    );
+pub fn get_diamond(letter: char) -> Vec<String> {
+    let peak = letter as i8 - b'A' as i8;
+    let edge = || (-peak..=peak).map(|i| i.abs());
+
+    edge()
+        .map(|x| {
+            edge()
+                .map(|y| if x + y == peak { char::from(b'A' + y as u8) } else { ' ' })
+                .collect()
+        })
+        .collect()
 }
