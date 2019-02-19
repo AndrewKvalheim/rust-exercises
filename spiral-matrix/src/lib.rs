2ab20e9b6343ef43ff3d76fa0b38b3985d9d3d31
Andrew Kvalheim <Andrew@Kvalhe.im>
2019-02-18 16:36:48 -0800

    Solve spiral-matrix.

--- spiral-matrix/src/lib.rs
+++ spiral-matrix/src/lib.rs
@@ -1,6 +1,19 @@
-pub fn spiral_matrix(size: u32) -> Vec<Vec<u32>> {
-    unimplemented!(
-        "Function that returns the spiral matrix of square size {}",
-        size
-    );
+const DIRECTIONS: [(isize, isize); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
+
+pub fn spiral_matrix(width: usize) -> Vec<Vec<u32>> {
+    let mut matrix = vec![vec![0; width]; width];
+    let mut n = 0;
+    let mut position = (-1, 0);
+
+    for (i, direction) in (1..(width * 2)).zip(DIRECTIONS.iter().cycle()) {
+        for _ in 0..((1 + width * 2 - i) / 2) {
+            n += 1;
+            position.0 += direction.0;
+            position.1 += direction.1;
+
+            matrix[position.1 as usize][position.0 as usize] = n;
+        }
+    }
+
+    matrix
 }
