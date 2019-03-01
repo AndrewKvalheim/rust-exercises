5351e8df1037acf3623e8254a564d2d0de602858
Andrew Kvalheim <Andrew@Kvalhe.im>
2019-03-01 08:41:15 -0800

    Solve roman-numerals.

--- roman-numerals/src/lib.rs
+++ roman-numerals/src/lib.rs
@@ -1,15 +1,47 @@
-use std::fmt::{Display, Formatter, Result};
+use std::fmt::{self, Display};
+use std::iter;
 
-pub struct Roman;
+const DIGITS: [(u32, &str); 13] = [
+    (1000, "M"),
+    (900, "CM"),
+    (500, "D"),
+    (400, "CD"),
+    (100, "C"),
+    (90, "XC"),
+    (50, "L"),
+    (40, "XL"),
+    (10, "X"),
+    (9, "IX"),
+    (5, "V"),
+    (4, "IV"),
+    (1, "I"),
+];
+
+pub struct Roman(u32);
+
+impl Roman {
+    fn digits(&self) -> impl Iterator<Item = &str> {
+        DIGITS
+            .iter()
+            .scan(self.0, |rest, &(value, symbol)| {
+                let count = *rest / value;
+
+                *rest %= value;
+
+                Some(iter::repeat(symbol).take(count as usize))
+            })
+            .flatten()
+    }
+}
 
 impl Display for Roman {
-    fn fmt(&self, _f: &mut Formatter<'_>) -> Result {
-        unimplemented!("Return a roman-numeral string representation of the Roman object");
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.digits().map(|d| f.write_str(d)).collect()
     }
 }
 
 impl From<u32> for Roman {
-    fn from(num: u32) -> Self {
-        unimplemented!("Construct a Roman object from the '{}' number", num);
+    fn from(n: u32) -> Self {
+        Self(n)
     }
 }
