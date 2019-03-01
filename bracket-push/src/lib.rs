65baca14e99de8fb2e77e04b31da3ec678ae9b69
Andrew Kvalheim <Andrew@Kvalhe.im>
2019-03-01 10:14:44 -0800

    Refine bracket-push.

--- bracket-push/src/lib.rs
+++ bracket-push/src/lib.rs
@@ -5,11 +5,7 @@ pub fn brackets_are_balanced(text: &str) -> bool {
     text.chars()
         .try_fold(Vec::new(), |mut due, character| {
             if let Some(&closing) = CLOSINGS.iter().find(|&&c| c == character) {
-                if due.pop() == Some(closing) {
-                    Some(due)
-                } else {
-                    None
-                }
+                due.pop().filter(|&d| d == closing).map(|_| due)
             } else {
                 if let Some(index) = OPENINGS.iter().position(|&o| o == character) {
                     due.push(CLOSINGS[index]);
