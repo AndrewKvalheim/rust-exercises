767f68a92de475d4748ab6bfdb0bd692c903a4a5
Andrew Kvalheim <Andrew@Kvalhe.im>
2019-04-15 10:26:42 -0700

    Solve luhn-trait.

--- luhn-trait/src/lib.rs
+++ luhn-trait/src/lib.rs
@@ -2,13 +2,8 @@ pub trait Luhn {
     fn valid_luhn(&self) -> bool;
 }
 
-/// Here is the example of how to implement custom Luhn trait
-/// for the &str type. Naturally, you can implement this trait
-/// by hand for the every other type presented in the test suite,
-/// but your solution will fail if a new type is presented.
-/// Perhaps there exists a better solution for this problem?
-impl<'a> Luhn for &'a str {
+impl<T: ToString> Luhn for T {
     fn valid_luhn(&self) -> bool {
-        unimplemented!("Determine if '{}' is a valid credit card number.", self);
+        luhn::is_valid(&self.to_string())
     }
 }
