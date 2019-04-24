f2ed57f585c20ff48235568690945fd1ba7cca68
Andrew Kvalheim <Andrew@Kvalhe.im>
2019-04-24 09:12:37 -0700

    Refine matching-brackets.

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -4,8 +4,8 @@ const CLOSINGS: [char; 3] = [')', ']', '}'];
 pub fn brackets_are_balanced(text: &str) -> bool {
     text.chars()
         .try_fold(Vec::new(), |mut due, character| {
-            if let Some(&closing) = CLOSINGS.iter().find(|&&c| c == character) {
-                due.pop().filter(|&d| d == closing).map(|_| due)
+            if CLOSINGS.contains(&character) {
+                due.pop().filter(|&d| d == character).map(|_| due)
             } else {
                 if let Some(index) = OPENINGS.iter().position(|&o| o == character) {
                     due.push(CLOSINGS[index]);
