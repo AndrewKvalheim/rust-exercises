3c86bd3cc44c2c993665559ed7a1593032b3b111
Andrew Kvalheim <Andrew@Kvalhe.im>
2019-07-23 09:10:02 -0700

    Solve sublist.

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering::*;
+
 #[derive(Debug, PartialEq)]
 pub enum Comparison {
     Equal,
@@ -6,6 +8,13 @@ pub enum Comparison {
     Unequal,
 }
 
-pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+pub fn sublist<T: PartialEq>(a: &[T], b: &[T]) -> Comparison {
+    let contains = |o: &[T], i: &[T]| i.is_empty() || o.windows(i.len()).any(|w| w == i);
+
+    match a.len().cmp(&b.len()) {
+        Equal if a == b => Comparison::Equal,
+        Greater if contains(a, b) => Comparison::Superlist,
+        Less if contains(b, a) => Comparison::Sublist,
+        _ => Comparison::Unequal,
+    }
 }
